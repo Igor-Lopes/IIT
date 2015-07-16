a01290975452bdd9712b231fbf2f14c510f9f39d
Igor Lopes <igor_olf@hotmail.com>
2015-07-15 22:09:50 -0500

    Create MemPointer.cpp

---
+++ MemPointer.cpp
@@ -0,0 +1,18 @@
+public: Process removeProcess(){
+	Process p;
+	if(size > 0){
+
+		if(header -> next == NULL){
+			p = header -> process;
+			header = NULL;
+		} else {
+			p = header -> process;
+			ListPtr new_header = new Node; // Leak
+			new_header = header -> next;
+			//header -> next = NULL; //Leak
+			header = new_header;
+			
+		}
+	size --;
+	return p;
+	}
