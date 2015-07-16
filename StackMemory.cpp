5eec43425d8b854081c8711298f4613021b073c7
Igor Lopes <igor_olf@hotmail.com>
2015-07-16 04:55:03 -0500

    Create StackMemory.cpp

---
+++ StackMemory.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+class Stack{
+
+  public: void showResult(double d){
+    double result;
+    result = 1 - d;
+    cout << "The result is:" << result<<endl;
+  }
+
+};
+
+
+int main (){
+Stack s;
+s.showResult(0.0163);
+
+return 0;
+}
